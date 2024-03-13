2f60fd120a895965d024713c6585a8302af2331d
likhitha <likhitha1911@gmail.com>
2024-03-13 15:31:56 +0530

    Extracting Company Name

--- invoice_carrier_extractor.py
+++ invoice_carrier_extractor.py
@@ -8,8 +8,6 @@ from PyPDF2 import PdfReader
 from pdf2image import convert_from_path
 import cv2
 import numpy as np
-import streamlit as st
-import tempfile
 
 path = f'C:/Program Files/poppler-23.11.0/Library/bin'
 
@@ -95,50 +93,3 @@ def extracted_data(invoice_text):
     company_name = chat_completion.choices[0].message.content
     print(company_name)
     return company_name
-
-# Streamlit UI
-def main():
-    st.set_page_config(page_title="Invoice Bot")
-    st.title("Invoice Data Extraction")
-
-    uploaded_files = st.file_uploader("Choose a PDF or image file", type=["pdf", "png", "jpg", "jpeg"], accept_multiple_files=True)
-    if uploaded_files is not None:
-        invoices = []
-        for uploaded_file in uploaded_files:
-            file_type = uploaded_file.name.split('.')[-1]
-            content = uploaded_file.read()
-
-            # Display uploaded image or PDF
-            if file_type.lower() in ['png', 'jpg', 'jpeg']:
-                text = extract_text_from_image(content)
-
-            elif file_type.lower() == 'pdf':
-                text = extract_text_from_pdf(content)
-
-            if text == "":
-                temp_dir = tempfile.TemporaryDirectory()
-                file_path = os.path.join(temp_dir.name, uploaded_file.name)
-
-                with open(file_path, 'wb') as f:
-                    f.write(content)
-
-                text = extract_text_from_pdf_img(file_path)
-
-                temp_dir.cleanup()
-            
-            invoices.append([uploaded_file.name,text])
-
-    else:
-        st.warning("Please upload a PDF or image file.")
-    
-    if st.button('Extract Data'):
-        st.subheader("Carrier name : ")
-
-        for name,text in invoices:
-            result = extracted_data(text)
-            st.write(name)
-            st.success(result)
-
-
-if __name__ == "__main__":
-    main()
